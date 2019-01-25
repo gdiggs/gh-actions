42a202fed40c1564b95259eafabdff155a1094ae
Gordon Diggs <gordon@gordondiggs.com>
2019-01-25 12:36:21 -0500

    Add cra-gh-pages
    Based heavily on https://github.com/stefanprodan/gh-actions/tree/master/helm-gh-pages

---
+++ cra-gh-pages/README.md
@@ -0,0 +1,5 @@
+# cra-gh-pages
+
+This is an action to compile and deploy a [Create React App] application to GitHub Pages
+
+[Create React App]: https://facebook.github.io/create-react-app
---
+++ cra-gh-pages/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+REPOSITORY="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+build() {
+  mkdir -p /github/home/pkg
+  yarn build
+  mv build /github/home/pkg
+}
+
+push() {
+  git config user.email ${GITHUB_ACTOR}@users.noreply.github.com
+  git config user.name ${GITHUB_ACTOR}
+  git remote set-url origin ${REPOSITORY}
+  git checkout -B gh-pages
+  mv /github/home/pkg/build/* .
+  git add .
+  git commit -m "Publish site"
+  git push origin gh-pages
+}
+
+if [[ "${GITHUB_REF}" == "master" ]]; then
+    echo "Starting action for master";
+else
+    echo "Skipping action because push was not to master" && exit 78;
+fi
+
+build
+push
